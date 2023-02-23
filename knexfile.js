23c20e9fdac66abfc8d8ad88feb81a3209e90c5b
Kobe <107245752+KPreme@users.noreply.github.com>
2023-02-23 15:26:30 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,17 +3,8 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  NODE_ENV = 'development', 
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL
-} = process.env;
-
-const URL = NODE_ENV === 'production' 
-? PRODUCTION_DATABASE_URL
-: DEVELOPMENT_DATABASE_URL;
-
-console.log(DEVELOPMENT_DATABASE_URL)
-console.log(PRODUCTION_DATABASE_URL)
+  URL = "postgres://nrfuwhbj:98pMC9tvvi-3E2KeAmBszFDB8IsNYMJO@batyr.db.elephantsql.com/nrfuwhbj",
+  } = process.env;
 
 module.exports = {
   development: {
